8abb4f8451cf930a375bdd245e34cf4cf762f327
RebelFlame <lucasrflame@gmail.com>
2019-02-07 15:37:22 +0000

    updated, razor pages

--- SimpleBlog/Global.asax.cs
+++ SimpleBlog/Global.asax.cs
@@ -5,12 +5,9 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace SimpleBlog
-{
-    public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
+namespace SimpleBlog {
+    public class MvcApplication : System.Web.HttpApplication {
+        protected void Application_Start() {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
